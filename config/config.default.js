2faba0ea942aa77a01287130f4ababab0113dc45
felixpy <felixpy.1993@gmail.com>
2018-11-29 00:00:20 +0800

    config: add mysql config

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1540464975987_7448';
@@ -12,6 +12,11 @@ module.exports = appInfo => {
   config.sequelize = {
     dialect: 'mysql',
     /* mysql config here */
+    database: 'gotauth',
+    host: 'localhost',
+    port: '3306',
+    username: process.env.GOT_DB_USR,
+    password: process.env.GOT_DB_PWD,
   };
 
   config.graphql = {
